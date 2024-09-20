4b3180b0a0cdcd469fd7e2b88a5bac4e7e55f62d
Chapatuman <suto0106dai@gmail.com>
2024-09-20 14:04:35 +0900

    InputSystem(´;ω;｀)

--- Assets/sound_cri/dir_change.cs
+++ Assets/sound_cri/dir_change.cs
@@ -6,22 +6,20 @@ public class dir_change : MonoBehaviour
 {
     [SerializeField]
     CriWare.Assets.CriAtomCueReference cueReference;
-    private Tmpg tmpg_;
     // Start is called before the first frame update
     void Start()
     {
-        tmpg_ = new Tmpg();
-        tmpg_.Enable();
+  
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(tmpg_.Player.Houkoukirikaehidari.triggered)
+        if(Input.GetButtonDown("KirikaeLeft"))
         {
             ADXSoundManager.Instance.PlaySound("dirchange", cueReference.AcbAsset.Handle, cueReference.CueId, gameObject.transform, false);
         }
-        else if (tmpg_.Player.Houkoukirikaemigi.triggered)
+        else if (Input.GetButtonDown("KirikaeRight"))
         {
             ADXSoundManager.Instance.PlaySound("dirchange", cueReference.AcbAsset.Handle, cueReference.CueId, gameObject.transform, false);
         }
