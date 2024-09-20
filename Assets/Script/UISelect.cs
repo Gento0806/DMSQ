eadc69b314daa6ba22882214e63aaa0469ab52a2
Chapatuman <suto0106dai@gmail.com>
2024-09-20 14:24:19 +0900

    InputSystem消去してInputManager(old)に統一

--- Assets/Script/UISelect.cs
+++ Assets/Script/UISelect.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 
 public class UIController : MonoBehaviour
 {
     // コントローラーでデフォルト選択されるUIボタンを指定
     public GameObject defaultSelectedButton;
+    private bool connected;
 
     void Update()
     {
+        var controllers = Input.GetJoystickNames();
+
+        if (!connected && controllers.Length > 0)
+        {
+            connected = true;
+            Debug.Log("Connected");
+
+        }
+        else if (connected && controllers.Length == 0)
+        {
+            connected = false;
+            Debug.Log("Disconnected");
+        }
+
         // コントローラーの入力を検知して、フォーカスをリセット
-        if (Gamepad.current != null && Gamepad.current.buttonSouth.wasPressedThisFrame)
+        if (connected)
         {
             // 現在何も選択されていない場合、デフォルトのボタンを選択する
             if (EventSystem.current.currentSelectedGameObject == null)
