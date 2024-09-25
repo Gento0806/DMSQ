c5f9a88cb3d1bf6ffd0208a6751364363cbc612b
cooolish26 <souta12266221@gmail.com>
2024-09-25 17:00:57 +0900

    クリアごの色変更（改）

--- Assets/Script/SceneSelect.cs
+++ Assets/Script/SceneSelect.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Reflection;
 using UnityEngine.UI;
+using Unity.VisualScripting;
+
 public class SceneSelect : MonoBehaviour
 {
     public static int StageNum = 0;
     public static bool[] ClearNum=new bool[20];
     [SerializeField] Sprite[] BeforeImage;
     [SerializeField] Sprite[] AfterImage;
+    [SerializeField] Sprite[] SelectedBeforeImage;
+    [SerializeField] Sprite[] SelectedAfterImage;
     public GameObject[] StageNumImage;
+    
     Image img;
+    Button btn;
     void Start()
     {
         if (SceneManager.GetActiveScene().buildIndex == 17&&this.gameObject.name== "StageButton1")//シーン選択画面のボタン１のみ読み込む
@@ -20,10 +26,21 @@ public class SceneSelect : MonoBehaviour
             for (int i = 1; i < StageNumImage.Length; i++)//クリア状況によって画像を入れ替える
             {
                 img = StageNumImage[i].GetComponent<Image>();
+
+                btn = StageNumImage[i].GetComponent<Button>();
+                SpriteState sp= btn.spriteState;
+
                 if (ClearNum[i])
+                {
                     img.sprite = AfterImage[i];
+                    sp.selectedSprite = SelectedAfterImage[i];
+                }
                 else
+                {
                     img.sprite = BeforeImage[i];
+                    sp.selectedSprite = SelectedBeforeImage[i];
+                }
+                btn.spriteState = sp;
             }
         }
 
